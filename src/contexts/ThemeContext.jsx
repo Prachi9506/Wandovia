475fcfe9fc6a2cc28bfbe9cee9071ba5a653c7e2
Prachi Sharma <prachimansoorpur@gmail.com>
2025-09-11 01:36:24 +0530

    Update ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,7 +1,7 @@
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const ThemeContext = createContext();
+const ThemeContext = createContext(); 
 
 const themes = {
   blue: {
